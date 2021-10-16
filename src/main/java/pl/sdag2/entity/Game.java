1e82b8d318e95ce8145ed93bba28442c7e8dd8f2
Super Grey <supergrey@outlook.com>
2021-10-16 15:42:22 +0200

    added fields for Game entity

--- src/main/java/pl/sdag2/entity/Game.java
+++ src/main/java/pl/sdag2/entity/Game.java
@@ -3,6 +3,7 @@ package pl.sdag2.entity;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.text.DecimalFormat;
 
 @Entity
 @Getter
@@ -10,5 +11,8 @@ public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String title;
+    private DecimalFormat priceFor24h;
+
 
 }
