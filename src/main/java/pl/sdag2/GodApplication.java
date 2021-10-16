5fb0e1b6f00a74dba1886544adc9860793f3d85d
Super Grey <supergrey@outlook.com>
2021-10-16 14:17:38 +0200

    Initial commit

---
+++ src/main/java/pl/sdag2/GodApplication.java
@@ -0,0 +1,13 @@
+package pl.sdag2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sdag2/GodApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdag2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
