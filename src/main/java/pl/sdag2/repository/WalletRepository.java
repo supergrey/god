e455aa22ea72f42152318f6fa46149106da4d55b
Paweł Żulewski <pazulewski@gmail.com>
2021-10-16 15:57:51 +0200

    edit WalletRepository

--- src/main/java/pl/sdag2/repository/WalletRepository.java
+++ src/main/java/pl/sdag2/repository/WalletRepository.java
@@ -1,6 +1,7 @@
 package pl.sdag2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.sdag2.entity.Wallet;
 
-public interface WalletRepository extends JpaRepository {
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
 }
