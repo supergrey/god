0ab4643b9ae42fcb764c88428474498734550204
Paweł Żulewski <pazulewski@gmail.com>
2021-10-16 14:36:59 +0200

    test end

--- src/main/java/pl/sdag2/GodApplication.java
+++ src/main/java/pl/sdag2/GodApplication.java
@@ -3,8 +3,6 @@ package pl.sdag2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//test
-
 @SpringBootApplication
 public class GodApplication {
 
