0276028a2fdf5baba9e9a10bde0b2e39d137a30d
Paweł Żulewski <pazulewski@gmail.com>
2021-10-16 15:41:37 +0200

    added getters and setters for Wallet

--- src/main/java/pl/sdag2/entity/Wallet.java
+++ src/main/java/pl/sdag2/entity/Wallet.java
@@ -1,11 +1,16 @@
 package pl.sdag2.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Wallet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
