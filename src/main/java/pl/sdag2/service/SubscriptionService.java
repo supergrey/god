638ff5b0ec40809375b4ff58a4b4b063bc026a19
Szajac99 <sebastian.zajac99@gmail.com>
2021-10-17 10:21:48 +0200

    dodaj funkcjonalność dodawania Subskrypcji

--- src/main/java/pl/sdag2/service/SubscriptionService.java
+++ src/main/java/pl/sdag2/service/SubscriptionService.java
@@ -20,4 +20,19 @@ public class SubscriptionService {
     public List<User> getAll() {
         return subscriptionRepository.findAll();
     }
+
+    public void createSubscription(User user, Subscription subscription) {
+        if (user.getLogin() == null ) {
+            IllegalArgumentException exception = new IllegalArgumentException("Nie znaleziono użytkownika");
+            log.error("Błąd tworzenia subskrypcji", exception);
+        } if (user.getEmail() == null) {
+            IllegalArgumentException exception2 = new IllegalArgumentException("Nie znaleziono adresu e-mail");
+            log.error("Błąd tworzenia subskrypcji", exception2);
+        } if (subscription.getId() != null) {
+            IllegalArgumentException exception3 = new IllegalArgumentException("Zadana subskrypcja już istnieje");
+            log.error("Błąd tworzenia subskrypcji", exception3);
+        }
+        subscriptionRepository.save(user);
+        log.info("Pomyślnie uzyskano subskrypcję!: " + subscription.getId());
+    }
 }
