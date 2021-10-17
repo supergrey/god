39c835ab31d7860ec985685349eadb253783e85c
Paweł Żulewski <pazulewski@gmail.com>
2021-10-17 13:07:24 +0200

    optimize modifying methods

--- src/main/java/pl/sdag2/repository/UserRepository.java
+++ src/main/java/pl/sdag2/repository/UserRepository.java
@@ -1,10 +1,7 @@
 package pl.sdag2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import pl.sdag2.entity.User;
-import pl.sdag2.entity.UserType;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
